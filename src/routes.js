c2bbf32411193781409a2d63daab00f06b4f9489
Antonio França <antoniofranca571@gmail.com>
2020-04-11 07:01:30 -0400

    update serverjs

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router =  express.Router();
-const toolController = require('./controller/ToolController');
+const authMiddleware = require('./app/middlewares/auth');
+const toolController = require('./app/controller/ToolController');
+const userController =  require('./app/controller/UserController');
+
+//Criação de Usuários
+router.post('/register',userController.create);
+
+//Autenticação de usuários
+router.post('/authenticate', userController.auth);
+
+//Chamada da autenticação 
+router.use(authMiddleware);
 
 //Listagem de ferramentas
 router.get('/tools',toolController.index);
