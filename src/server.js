48c976d4a0d0591adbae0f15e4ab97f48c2be266
Antonio França <antoniofranca571@gmail.com>
2020-09-03 19:11:04 -0400

    update files

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/dbvuttr',{
     useNewUrlParser: true,
     useCreateIndex: true
 });
+
 mongoose.set('useFindAndModify', false);
 
 //Configuração para as models
